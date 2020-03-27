b5142c683509f03b1a055cca0540c93ab7948004
mwl10 <matthewlowery@Matthews-MacBook-Pro.local>
2020-03-27 13:01:06 -0400

    renamed files properly

--- ark-tweet-nlp-0.3.2/twtt.py
+++ ark-tweet-nlp-0.3.2/twtt.py
@@ -20,21 +20,21 @@ for tweet in tweet_lines:
     tweet = tweet.split("\t")[1]
     tweets.write(tweet)
 
-# make sure the tagged tweet file is empty to write to
-tagged_tweets.truncate(0)
-
-# redirect runtagger output to the tagged tweets file
-runTagger_shell = "./runTagger.sh --model model.ritter_ptb_alldata_fixed.20130723 {} > {}".format(sys.argv[1], sys.argv[3])
-os.system(runTagger_shell)
-
-# get the lines from the tags
-tagged_tweet_lines = tagged_tweets.readlines()
-
-# make sure nothings in the output file first
+# make sure the output_ark file is empty to write to
 output_ark.truncate(0)
 
+# redirect runtagger output to the ark_output file
+runTagger_shell = "./runTagger.sh --model model.ritter_ptb_alldata_fixed.20130723 {} > {}".format(sys.argv[1], sys.argv[2])
+os.system(runTagger_shell)
+
+# get the lines from the ark output
+output_ark_lines = output_ark.readlines()
+
+# make sure nothings in the tagged output file first
+tagged_tweets.truncate(0)
+
 line_index = 0
-for line in tagged_tweet_lines:
+for line in output_ark_lines:
     tagged_tokens = " "
     ark_tag_list = list(line.split("\t"))
     # list of words from this particular tweet
@@ -51,7 +51,7 @@ for line in tagged_tweet_lines:
     # add the sentiment for this particular tweet
     sent_tag_token = tweet_lines[line_index].split("\t")[0] + "\t" + tagged_tokens + "\n"
     # write it to the output file
-    output_ark.write(sent_tag_token)
+    tagged_tweets.write(sent_tag_token)
     line_index += 1
 
 # close the files
